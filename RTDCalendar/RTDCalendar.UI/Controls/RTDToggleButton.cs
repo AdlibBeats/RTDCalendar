5340ac209fc1823199fca4c3e855136d41be5e23
Vasilev <adlibbeats@hotmail.com>
2018-05-18 17:49:28 +0300

    577659996556565656

---
+++ RTDCalendar/RTDCalendar.UI/Controls/RTDToggleButton.cs
@@ -0,0 +1,185 @@
+﻿using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
+
+// ReSharper disable once CheckNamespace
+namespace RTDCalendar.UI.Controls
+{
+	public class RTDToggleButton : ToggleButton
+	{
+		private Border _contentBorder;
+
+		public RTDToggleButton()
+		{
+			DefaultStyleKey = typeof(RTDToggleButton);
+
+			SizeChanged += OnSizeChanged;
+		}
+		private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+		{
+			UpdateCornerRadius();
+		}
+
+		protected override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+
+			_contentBorder = GetTemplateChild("ContentBorder") as Border;
+
+			UpdateCornerRadius();
+		}
+
+		private void UpdateCornerRadius()
+		{
+			if (_contentBorder == null)
+				return;
+			if (IsCornerRadiusVisible)
+				_contentBorder.CornerRadius =
+					CornerRadius.Equals(default(CornerRadius)) ? new CornerRadius(ActualHeight / 2) : CornerRadius;
+			else
+				_contentBorder.CornerRadius = default(CornerRadius);
+		}
+
+		public DataTemplate OnCheckedTemplate
+		{
+			get => (DataTemplate)GetValue(OnCheckedTemplateProperty);
+			set => SetValue(OnCheckedTemplateProperty, value);
+		}
+
+		public static readonly DependencyProperty OnCheckedTemplateProperty =
+			DependencyProperty.Register(nameof(OnCheckedTemplate), typeof(DataTemplate), typeof(RTDToggleButton),
+				new PropertyMetadata(default(DataTemplate)));
+
+		public object OnCheckedContent
+		{
+			get => GetValue(OnCheckedContentProperty);
+			set => SetValue(OnCheckedContentProperty, value);
+		}
+
+		public static readonly DependencyProperty OnCheckedContentProperty =
+			DependencyProperty.Register(nameof(OnCheckedContent), typeof(object), typeof(RTDToggleButton),
+				new PropertyMetadata(default(object)));
+
+		public Brush OnCheckedBackground
+		{
+			get => (Brush)GetValue(OnCheckedBackgroundProperty);
+			set => SetValue(OnCheckedBackgroundProperty, value);
+		}
+
+		public static readonly DependencyProperty OnCheckedBackgroundProperty =
+			DependencyProperty.Register(nameof(OnCheckedBackground), typeof(Brush), typeof(RTDToggleButton),
+				new PropertyMetadata(new SolidColorBrush(Color.FromArgb(255, 237, 28, 36))));
+
+		public Brush OnCheckedForeground
+		{
+			get => (Brush)GetValue(OnCheckedForegroundProperty);
+			set => SetValue(OnCheckedForegroundProperty, value);
+		}
+
+		public static readonly DependencyProperty OnCheckedForegroundProperty =
+			DependencyProperty.Register(nameof(OnCheckedForeground), typeof(Brush), typeof(RTDToggleButton),
+				new PropertyMetadata(new SolidColorBrush(Color.FromArgb(255, 255, 255, 255))));
+
+		public Brush OnCheckedBorderBrush
+		{
+			get => (Brush)GetValue(OnCheckedBorderBrushProperty);
+			set => SetValue(OnCheckedBorderBrushProperty, value);
+		}
+
+		public static readonly DependencyProperty OnCheckedBorderBrushProperty =
+			DependencyProperty.Register(nameof(OnCheckedBorderBrush), typeof(Brush), typeof(RTDToggleButton),
+				new PropertyMetadata(default(Brush)));
+
+		public Thickness OnCheckedBorderThickness
+		{
+			get => (Thickness)GetValue(OnCheckedBorderThicknessProperty);
+			set => SetValue(OnCheckedBorderThicknessProperty, value);
+		}
+
+		public static readonly DependencyProperty OnCheckedBorderThicknessProperty =
+			DependencyProperty.Register(nameof(OnCheckedBorderThickness), typeof(Thickness), typeof(RTDToggleButton),
+				new PropertyMetadata(new Thickness(0)));
+
+		public DataTemplate OnDisabledTemplate
+		{
+			get => (DataTemplate)GetValue(OnDisabledTemplateProperty);
+			set => SetValue(OnDisabledTemplateProperty, value);
+		}
+
+		public static readonly DependencyProperty OnDisabledTemplateProperty =
+			DependencyProperty.Register(nameof(OnDisabledTemplate), typeof(DataTemplate), typeof(RTDToggleButton),
+				new PropertyMetadata(default(DataTemplate)));
+
+		public object OnDisabledContent
+		{
+			get => GetValue(OnDisabledContentProperty);
+			set => SetValue(OnDisabledContentProperty, value);
+		}
+
+		public static readonly DependencyProperty OnDisabledContentProperty =
+			DependencyProperty.Register(nameof(OnDisabledContent), typeof(object), typeof(RTDToggleButton),
+				new PropertyMetadata(default(object)));
+
+		public Brush OnDisabledBackground
+		{
+			get => (Brush)GetValue(OnDisabledBackgroundProperty);
+			set => SetValue(OnDisabledBackgroundProperty, value);
+		}
+
+		public static readonly DependencyProperty OnDisabledBackgroundProperty =
+			DependencyProperty.Register(nameof(OnDisabledBackground), typeof(Brush), typeof(RTDToggleButton),
+				new PropertyMetadata(new SolidColorBrush(Color.FromArgb(255, 237, 28, 36))));
+
+		public Brush OnDisabledForeground
+		{
+			get => (Brush)GetValue(OnDisabledForegroundProperty);
+			set => SetValue(OnDisabledForegroundProperty, value);
+		}
+
+		public static readonly DependencyProperty OnDisabledForegroundProperty =
+			DependencyProperty.Register(nameof(OnDisabledForeground), typeof(Brush), typeof(RTDToggleButton),
+				new PropertyMetadata(new SolidColorBrush(Color.FromArgb(255, 255, 255, 255))));
+
+		public Brush OnDisabledBorderBrush
+		{
+			get => (Brush)GetValue(OnDisabledBorderBrushProperty);
+			set => SetValue(OnDisabledBorderBrushProperty, value);
+		}
+
+		public static readonly DependencyProperty OnDisabledBorderBrushProperty =
+			DependencyProperty.Register(nameof(OnDisabledBorderBrush), typeof(Brush), typeof(RTDToggleButton),
+				new PropertyMetadata(default(Brush)));
+
+		public Thickness OnDisabledBorderThickness
+		{
+			get => (Thickness)GetValue(OnDisabledBorderThicknessProperty);
+			set => SetValue(OnDisabledBorderThicknessProperty, value);
+		}
+
+		public static readonly DependencyProperty OnDisabledBorderThicknessProperty =
+			DependencyProperty.Register(nameof(OnDisabledBorderThickness), typeof(Thickness), typeof(RTDToggleButton),
+				new PropertyMetadata(new Thickness(0)));
+
+		public bool IsCornerRadiusVisible
+		{
+			get => (bool)GetValue(IsCornerRadiusVisibleProperty);
+			set => SetValue(IsCornerRadiusVisibleProperty, value);
+		}
+
+		public static readonly DependencyProperty IsCornerRadiusVisibleProperty =
+			DependencyProperty.Register(nameof(IsCornerRadiusVisible), typeof(bool), typeof(RTDToggleButton),
+				new PropertyMetadata(false, (d, e) => ((RTDToggleButton)d).UpdateCornerRadius()));
+
+		public CornerRadius CornerRadius
+		{
+			get => (CornerRadius)GetValue(CornerRadiusProperty);
+			set => SetValue(CornerRadiusProperty, value);
+		}
+
+		public static readonly DependencyProperty CornerRadiusProperty =
+			DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(RTDToggleButton),
+				new PropertyMetadata(default(CornerRadius), (d, e) => ((RTDToggleButton)d).UpdateCornerRadius()));
+	}
+}
---
+++ RTDCalendar/RTDCalendar.UI/Helpers/DayCompositor.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace RTDCalendar.UI.Helpers
+{
+    public class DayCompositor
+    {
+        // день как единица...фазирование на три месяца...
+        // кнопки назад и вперед
+        // месяц из нескольких дней
+        // год из 12 месяцев
+        // 
+
+
+    }
+
+    public interface IDate
+    {
+        int Unit { get; set; }
+    }
+
+    public class Date : IDate
+    {
+        private int _unit;
+        public int Unit
+        {
+            get => _unit;
+            set => _unit = value;
+        }
+    }
+
+    public class Day : Date
+    {
+
+    }
+
+    public class Month : Date
+    {
+
+    }
+
+    public class Year : Date
+    {
+
+    }
+
+    public interface INavigationDate<T>
+    {
+        bool GoToNextDate(Type dateType);
+        bool GoToPreviousDate(Type dateType);
+        bool GoToDate(T date);
+    }
+
+    public interface ICurrentDate<T>
+    {
+        T CurrentYear { get; }
+        T CurrentMonth { get; }
+    }
+
+    public interface IDateStorage<T>
+    {
+        List<T> LastDates { get; }
+        List<T> CurrentDates { get; }
+        List<T> NextDates { get; }
+
+        List<T> GetDatesAtMonth(T year, T month);
+    }
+
+    public class DateStorage<T> : INavigationDate<T>, ICurrentDate<T>, IDateStorage<T> where T : Date, new()
+    {
+        public T CurrentYear { get; }
+        public T CurrentMonth { get; }
+
+        public List<T> LastDates { get; }
+        public List<T> CurrentDates { get; }
+        public List<T> NextDates { get; }
+
+        public DateStorage(T year = default(T), T month = default(T))
+        {
+            InitializeDate(ref year, ref month);
+
+            CurrentYear = year;
+            CurrentMonth = month;
+
+            LastDates = GetDatesAtMonth(year, new T { Unit = MonthHelper.GetLastMonth(month.Unit) });
+            CurrentDates = GetDatesAtMonth(year, new T { Unit = MonthHelper.GetCurrentMonth(month.Unit) });
+            NextDates = GetDatesAtMonth(year, new T { Unit = MonthHelper.GetNextMonth(month.Unit) });
+        }
+
+        private void InitializeDate(ref T year, ref T month)
+        {
+            if (year == default(T) || year.Unit < 1)
+                year = new T { Unit = DateTime.Now.Year };
+            if (month == default(T) || month.Unit < 1)
+                month = new T { Unit = DateTime.Now.Month };
+        }
+
+        public List<T> GetDatesAtMonth(T year, T month)
+        {
+            List<T> listDates = new List<T>();
+            int daysInMonth = DateTime.DaysInMonth(year.Unit, month.Unit);
+
+            for (int day = 1; day <= daysInMonth; day++)
+                listDates.Add(new T { Unit = day });
+
+            return listDates;
+        }
+
+        public bool GoToNextDate(Type dateType)
+        {
+            return true;
+        }
+
+        public bool GoToPreviousDate(Type dateType)
+        {
+            return true;
+        }
+
+        public bool GoToDate(T date)
+        {
+            return true;
+        }
+    }
+
+    public static class MonthHelper
+    {
+        public static int GetLastMonth(int month)
+        {
+            if (month < 2)
+                return 12;
+            else if (month > 11)
+                return 11;
+            else
+                return month - 1;
+        }
+
+        public static int GetCurrentMonth(int month)
+        {
+            if (month < 1)
+                return 1;
+            else if (month > 11)
+                return 12;
+            else
+                return month;
+        }
+
+        public static int GetNextMonth(int month)
+        {
+            if (month > 11)
+                return 1;
+            else if (month < 1)
+                return 1;
+            else
+                return month + 1;
+        }
+    }
+
+    // Текущий месяц
+}
